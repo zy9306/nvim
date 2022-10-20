997816cb90a0524853ec985d7e0422fd16203a1c
Illyasviel <zy99306@outlook.com>
2022-10-20 09:36:13 +0800

    adjust key

--- lua/init-telescope.lua
+++ lua/init-telescope.lua
@@ -1,16 +1,16 @@
 -- telescope
 local telescope = require('telescope')
 
-vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>fs", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>ft", "<cmd>lua require('telescope.builtin').treesitter()<cr>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>fx", "<cmd>lua require('telescope.builtin').commands()<cr>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>tf", "<cmd>lua require('telescope.builtin').find_files()<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>tF", ":Telescope file_browser path=%:p:h<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>tb", "<cmd>lua require('telescope.builtin').buffers()<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>ts", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>lua require('telescope.builtin').treesitter()<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>tx", "<cmd>lua require('telescope.builtin').commands()<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>th", "<cmd>lua require('telescope.builtin').help_tags()<cr>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>lua require('telescope').extensions.frecency.frecency()<cr>",
                         {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>fl", ":Telescope file_browser path=%:p:h<cr>", {noremap = true})
 
 telescope.load_extension('fzf')
 telescope.load_extension("frecency")
