466af661d353c966a6b62b88a7a7956513ff6766
Illyasviel <zy99306@outlook.com>
2024-09-28 16:46:14 +0800

    add lsp_signature.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,4 +11,12 @@ return {
 			})
 		end,
 	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {},
+		config = function(_, opts)
+			require("lsp_signature").setup(opts)
+		end,
+	},
 }
