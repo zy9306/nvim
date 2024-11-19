5118ef223a2fa38a33cfadc93c1fda77ff652376
Illyasviel <zy99306@outlook.com>
2024-11-19 14:50:50 +0800

    add famiu/bufdelete.nvim,j-morano/buffer_manager.nvim

---
+++ lua/plugins/buffer.lua
@@ -0,0 +1,20 @@
+return {
+	-- {
+	-- 	"famiu/bufdelete.nvim",
+	-- 	event = "BufEnter",
+	-- },
+
+	{
+		"j-morano/buffer_manager.nvim",
+		event = "BufEnter",
+		config = function()
+			require("buffer_manager").setup({})
+			vim.keymap.set(
+				"n",
+				"<leader>B",
+				':lua require("buffer_manager.ui").toggle_quick_menu()<cr>',
+				{ noremap = true, silent = true, desc = "Buffer Manager" }
+			)
+		end,
+	},
+}
