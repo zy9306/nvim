e094b0ec21834b8a839b8620051bf7a9d8a875db
Illyasviel <zy99306@outlook.com>
2024-12-04 12:48:30 +0800

    adjust render-markdown

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -30,6 +30,9 @@ return {
 				"MeanderingProgrammer/render-markdown.nvim",
 				opts = {
 					file_types = { "markdown", "Avante" },
+					heading = {
+						enabled = false,
+					},
 				},
 				ft = { "markdown", "Avante" },
 			},
