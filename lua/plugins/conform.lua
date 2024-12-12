66f4b223a4a6aa8d71c9e82d936f4e33968a718c
Illyasviel <zy99306@outlook.com>
2024-12-12 10:49:32 +0800

    adjust conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -20,7 +20,7 @@ return {
 
 				format_after_save = function(bufnr)
 					local bufname = vim.api.nvim_buf_get_name(bufnr)
-					local patterns = { "/C_ASTHost/", "/C_ASTHost_test/" }
+					local patterns = {}
 
 					for _, pattern in ipairs(patterns) do
 						if bufname:match(pattern) then
