4afa7db2396d4cb7ca6e3f37a5e24e384aa3fbad
Illyasviel <zy99306@outlook.com>
2024-12-28 19:14:30 +0800

    update vscode config

--- lua/init-vscode.lua
+++ lua/init-vscode.lua
@@ -3,4 +3,8 @@ local opts = {
 	silent = true,
 }
 
-vim.keymap.set({ "n", "v" }, "<leader>ff", ":lua require('vscode').action('workbench.action.quickOpen')<cr>", opts)
+vim.keymap.set({ "n" }, "<leader>ff", ":lua require('vscode').action('workbench.action.quickOpen')<cr>", opts)
+vim.keymap.set({ "n" }, "<leader>b", ":lua require('vscode').action('workbench.action.quickOpen')<cr>", opts)
+vim.keymap.set({ "n" }, "<leader>x", ":lua require('vscode').action('workbench.action.showCommands')<cr>", opts)
+vim.keymap.set({ "n" }, "<leader>w", ":lua require('vscode').action('workbench.action.files.save')<cr>", opts)
+vim.keymap.set({ "n" }, "<leader>S", ":lua require('vscode').action('workbench.action.findInFiles')<cr>", opts)
