14034df06b27f28c291f59f3f594ecb4a0d61131
Illyasviel <zy99306@outlook.com>
2025-01-10 10:55:16 +0800

    adjust themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -25,6 +25,10 @@ return {
         config = function()
             require("catppuccin").setup({
                 flavour = "auto",
+                background = {
+                    light = "latte",
+                    dark = "mocha",
+                },
             })
             vim.cmd.colorscheme("catppuccin")
         end,
