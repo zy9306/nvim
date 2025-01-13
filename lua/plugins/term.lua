9246fe3e68d539e69d30d9181bdb9a455068b927
Illyasviel <zy99306@outlook.com>
2025-01-13 18:08:43 +0800

    adjust term

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -20,6 +20,38 @@ return {
             vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
             vim.keymap.set("n", "<C-t>", ":ToggleTerm direction=horizontal<CR>", opts)
             vim.keymap.set("t", "<C-t>", "<C-\\><C-n>:ToggleTerm direction=horizontal<CR>", opts)
+
+            local terms = require("toggleterm.terminal")
+            vim.api.nvim_create_autocmd("BufEnter", {
+                pattern = "term://*",
+                callback = function()
+                    if vim.fn.mode() ~= "t" then
+                        print("Entering terminal mode")
+                        local _, term = terms.identify()
+                        if term then
+                            term:set_mode(terms.mode.INSERT)
+                        end
+                    end
+                end,
+            })
+
+            vim.api.nvim_create_autocmd("ModeChanged", {
+                pattern = "v:*",
+                callback = function()
+                    if vim.bo.buftype == "terminal" then
+                        local _, term = terms.identify()
+                        if term then
+                            term:set_mode(terms.mode.INSERT)
+                        end
+                    end
+                end,
+            })
+
+            vim.api.nvim_create_autocmd("TermOpen", {
+                callback = function()
+                    vim.keymap.set("n", "q", "<Cmd>startinsert<CR>", { buffer = true, noremap = true, silent = true })
+                end,
+            })
         end,
     },
 }
