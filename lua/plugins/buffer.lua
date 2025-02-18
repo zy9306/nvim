261f4ec2a2691c64dde256c595576b5a017f69e0
Illyasviel <zy99306@outlook.com>
2025-02-18 09:41:35 +0800

    winfixbuf

--- lua/plugins/buffer.lua
+++ lua/plugins/buffer.lua
@@ -1,9 +1,15 @@
-return {
-    -- {
-    -- 	"famiu/bufdelete.nvim",
-    -- 	event = "BufEnter",
-    -- },
+function toggle_winfixbuf()
+    vim.wo.winfixbuf = not vim.wo.winfixbuf
+    if vim.wo.winfixbuf then
+        print("winfixbuf enabled")
+    else
+        print("winfixbuf disabled")
+    end
+end
 
+vim.api.nvim_create_user_command("ToggleWinfixbuf", toggle_winfixbuf, {})
+
+return {
     {
         "j-morano/buffer_manager.nvim",
         event = "BufEnter",
@@ -15,14 +21,6 @@ return {
                 ':lua require("buffer_manager.ui").toggle_quick_menu()<cr>',
                 { noremap = true, silent = true, desc = "Buffer Manager" }
             )
-        end,
-    },
-
-    {
-        "stevearc/stickybuf.nvim",
-        event = "BufEnter",
-        config = function()
-            require("stickybuf").setup({})
         end,
     },
 
