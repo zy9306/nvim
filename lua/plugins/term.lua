8a2cd9d3b158f73f8d17f41a9dbdc110ac16ee2c
Illyasviel <zy99306@outlook.com>
2025-02-18 12:56:22 +0800

    delete willothy/flatten.nvim, it break neotest

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -77,17 +77,4 @@ return {
             })
         end,
     },
-
-    {
-        "willothy/flatten.nvim",
-        lazy = false,
-        priority = 1001,
-        config = function()
-            require("flatten").setup({
-                window = {
-                    open = "alternate",
-                },
-            })
-        end,
-    },
 }
