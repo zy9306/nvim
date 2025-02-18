f468b0c7c4570b005031f75222546c53461deefa
Illyasviel <zy99306@outlook.com>
2025-02-18 16:44:13 +0800

    disable bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,27 +1,27 @@
 return {
-    {
-        "akinsho/bufferline.nvim",
-        version = "*",
-        event = "BufReadPre",
-        dependencies = "nvim-tree/nvim-web-devicons",
-        config = function()
-            require("bufferline").setup({
-                options = {
-                    show_buffer_close_icons = false,
-                    show_close_icon = false,
-                    diagnostics = "nvim_lsp",
-                    offsets = {
-                        {
-                            filetype = "NvimTree",
-                            text = "File Explorer",
-                            text_align = "left",
-                            separator = true,
-                        },
-                    },
-                },
-            })
-        end,
-    },
+    -- {
+    --     "akinsho/bufferline.nvim",
+    --     version = "*",
+    --     event = "BufReadPre",
+    --     dependencies = "nvim-tree/nvim-web-devicons",
+    --     config = function()
+    --         require("bufferline").setup({
+    --             options = {
+    --                 show_buffer_close_icons = false,
+    --                 show_close_icon = false,
+    --                 diagnostics = "nvim_lsp",
+    --                 offsets = {
+    --                     {
+    --                         filetype = "NvimTree",
+    --                         text = "File Explorer",
+    --                         text_align = "left",
+    --                         separator = true,
+    --                     },
+    --                 },
+    --             },
+    --         })
+    --     end,
+    -- },
     -- {
     --     "willothy/nvim-cokeline",
     --     event = "BufReadPre",
