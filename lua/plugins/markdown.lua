205767527048614338c04fcabc6c841b1c80e496
Illyasviel <zy99306@outlook.com>
2025-02-27 11:32:28 +0800

    adjust

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+            file_types = { "markdown", "Avante" },
+            heading = {
+                enabled = false,
+            },
+        },
+        ft = { "markdown", "Avante" },
+    },
+}
