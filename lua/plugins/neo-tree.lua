8e7d67e5f6ce6dbda2794d28e35029b0e06fd1ae
Illyasviel <zy99306@outlook.com>
2025-03-10 15:14:40 +0800

    avante neotree shortcut

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -31,10 +31,32 @@ return {
                     },
                 },
                 filesystem = {
+                    commands = {
+                        avante_add_files = function(state)
+                            local node = state.tree:get_node()
+                            local filepath = node:get_id()
+                            local relative_path = require("avante.utils").relative_path(filepath)
+
+                            local sidebar = require("avante").get()
+
+                            local open = sidebar:is_open()
+                            if not open then
+                                require("avante.api").ask()
+                                sidebar = require("avante").get()
+                            end
+
+                            sidebar.file_selector:add_selected_file(relative_path)
+
+                            if not open then
+                                sidebar.file_selector:remove_selected_file("neo-tree filesystem [1]")
+                            end
+                        end,
+                    },
                     window = {
                         mappings = {
                             ["bd"] = "noop",
                             ["D"] = "delete",
+                            ["oa"] = "avante_add_files",
                         },
                     },
                 },
