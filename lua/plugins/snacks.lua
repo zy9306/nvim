d0a7eb7e3ed9090405663483eab320a08289eb78
Illyasviel <zy99306@outlook.com>
2025-03-14 14:40:14 +0800

    delete dressing.nvim, use snacks.nvim

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,5 +3,9 @@ return {
         "folke/snacks.nvim",
         priority = 1000,
         lazy = false,
+        opts = {
+            input = { enabled = true },
+            picker = { enabled = true },
+        },
     },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,13 +1,4 @@
 return {
-    -- or use nvim-telescope/telescope-ui-select.nvim
-    {
-        "stevearc/dressing.nvim",
-        event = "VeryLazy",
-        opts = {},
-        config = function()
-            require("dressing").setup({})
-        end,
-    },
     {
         "nvim-zh/colorful-winsep.nvim",
         event = { "WinLeave" },
