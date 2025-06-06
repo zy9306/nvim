fa49f8870881ba6d1b27c3049fecc4ebb3d70164
Illyasviel <zy99306@outlook.com>
2025-06-06 10:27:31 +0800

    update

--- lua/plugins/auto_save.lua
+++ lua/plugins/auto_save.lua
@@ -3,7 +3,12 @@ local auto_save = {}
 function auto_save.save()
     local buf = vim.api.nvim_get_current_buf()
     local buf_name = vim.api.nvim_buf_get_name(buf)
-    if buf_name ~= "" and not string.find(buf_name, "quickfix") and vim.bo[buf].modified then
+    if
+        buf_name ~= ""
+        and not string.find(buf_name, "quickfix")
+        and not string.find(buf_name, "tmp")
+        and vim.bo[buf].modified
+    then
         vim.cmd("write")
         require("conform").format({ async = true })
     end
