e3f893a85a1651699b49ab264a7d7a41bdc1f9c4
Illyasviel <zy99306@outlook.com>
2025-08-01 16:04:17 +0800

    adjust auto-save

--- lua/plugins/auto_save.lua
+++ lua/plugins/auto_save.lua
@@ -1,39 +1,48 @@
-local auto_save = {}
+-- local auto_save = {}
+--
+-- function auto_save.save()
+--     local buf = vim.api.nvim_get_current_buf()
+--     local buf_name = vim.api.nvim_buf_get_name(buf)
+--     if
+--         buf_name ~= ""
+--         and not string.find(buf_name, "quickfix")
+--         and not string.find(buf_name, "tmp")
+--         and vim.bo[buf].modified
+--     then
+--         vim.cmd("write")
+--         require("conform").format({ async = true })
+--     end
+-- end
+--
+-- function auto_save.safe_save()
+--     local status, err = pcall(auto_save.save)
+--     if not status then
+--         vim.api.nvim_err_writeln("AutoSave Error: " .. err)
+--     end
+-- end
+--
+-- function auto_save.setup()
+--     vim.api.nvim_create_autocmd({ "InsertLeave", "BufLeave" }, {
+--         callback = auto_save.safe_save,
+--     })
+--     vim.api.nvim_create_autocmd("ModeChanged", {
+--         pattern = "i:*",
+--         callback = auto_save.safe_save,
+--     })
+--     vim.api.nvim_create_autocmd("TextChanged", {
+--         callback = auto_save.safe_save,
+--     })
+-- end
+--
+-- auto_save.setup()
 
-function auto_save.save()
-    local buf = vim.api.nvim_get_current_buf()
-    local buf_name = vim.api.nvim_buf_get_name(buf)
-    if
-        buf_name ~= ""
-        and not string.find(buf_name, "quickfix")
-        and not string.find(buf_name, "tmp")
-        and vim.bo[buf].modified
-    then
-        vim.cmd("write")
-        require("conform").format({ async = true })
-    end
-end
-
-function auto_save.safe_save()
-    local status, err = pcall(auto_save.save)
-    if not status then
-        vim.api.nvim_err_writeln("AutoSave Error: " .. err)
-    end
-end
-
-function auto_save.setup()
-    vim.api.nvim_create_autocmd({ "InsertLeave", "BufLeave" }, {
-        callback = auto_save.safe_save,
-    })
-    vim.api.nvim_create_autocmd("ModeChanged", {
-        pattern = "i:*",
-        callback = auto_save.safe_save,
-    })
-    vim.api.nvim_create_autocmd("TextChanged", {
-        callback = auto_save.safe_save,
-    })
-end
-
-auto_save.setup()
-
-return {}
+return {
+    {
+        "okuuva/auto-save.nvim",
+        cmd = "ASToggle",
+        event = { "InsertLeave", "TextChanged" },
+        config = function()
+            require("auto-save").setup()
+        end,
+    },
+}
