900bd186b5f760594efa307e9a3adc3212d9fbc5
Illyasviel <zy99306@outlook.com>
2025-08-11 16:43:20 +0800

    adjust neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -22,16 +22,17 @@ return {
         config = function()
             vim.keymap.set("n", "<F9>", ":Neotree source=buffers toggle=true<cr>", { desc = "Toggle Neotree buffers" })
 
-            vim.keymap.set(
-                "n",
-                "<F8>",
-                ":Neotree toggle=true position=float<cr>",
-                { desc = "Toggle Neotree filesystem" }
-            )
+            vim.keymap.set("n", "<F8>", ":Neotree toggle=true<cr>", { desc = "Toggle Neotree filesystem" })
+
             require("neo-tree").setup({
+                enable_opened_markers = true,
+                default_component_configs = {
+                    name = {
+                        highlight_opened_files = "all",
+                    },
+                },
                 window = {
                     width = 35,
-                    -- position = "float",
                     mappings = {
                         ["E"] = function()
                             vim.api.nvim_exec("Neotree focus filesystem left", true)
@@ -49,33 +50,15 @@ return {
                     },
                 },
                 filesystem = {
-                    commands = {
-                        avante_add_files = function(state)
-                            local node = state.tree:get_node()
-                            local filepath = node:get_id()
-                            local relative_path = require("avante.utils").relative_path(filepath)
-
-                            local sidebar = require("avante").get()
-
-                            local open = sidebar:is_open()
-                            if not open then
-                                require("avante.api").ask()
-                                sidebar = require("avante").get()
-                            end
-
-                            sidebar.file_selector:add_selected_file(relative_path)
-
-                            if not open then
-                                sidebar.file_selector:remove_selected_file("neo-tree filesystem [1]")
-                            end
-                        end,
-                    },
                     window = {
                         mappings = {
                             ["bd"] = "noop",
                             ["D"] = "delete",
-                            ["oa"] = "avante_add_files",
                         },
+                    },
+                    follow_current_file = {
+                        enabled = true,
+                        leave_dirs_open = true,
                     },
                 },
                 buffers = {
