abd780eab6a443039cb2d6f304246848a074d6c1
Illyasviel <zy99306@outlook.com>
2025-08-12 15:45:46 +0800

    add lazygit

--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -35,6 +35,25 @@ return {
     },
 
     {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        },
+    },
+
+    {
         "tpope/vim-fugitive",
     },
 
