1bd3784872bafd8766c27dc815cce102756039a0
Illyasviel <zy99306@outlook.com>
2025-08-20 16:45:56 +0800

    adjust oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,7 +4,17 @@ return {
         opts = {},
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "oil",
+                callback = function(args)
+                    vim.keymap.set("n", "<C-c>", "<Esc>", { buffer = args.buf, noremap = true, silent = true })
+                    vim.keymap.set("i", "<C-c>", "<Esc>", { buffer = args.buf, noremap = true, silent = true })
+                end,
+            })
+
             require("oil").setup({
+                -- :Oil --trash / to view trash
+                delete_to_trash = true,
                 view_options = {
                     show_hidden = true,
                 },
