190bcfc0546fdfb83eca009f518710069fb519bc
Illyasviel <zy99306@outlook.com>
2025-08-25 10:55:38 +0800

    update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -33,6 +33,7 @@ return {
                     typescriptreact = { "prettierd", "prettier", stop_after_first = true },
                     svelte = { "prettierd", "prettier", stop_after_first = true },
                     html = { "prettierd", "prettier", stop_after_first = true },
+                    css = { "prettierd", "prettier", stop_after_first = true },
                     json = { "prettierd", "prettier", stop_after_first = true },
                     sh = { "shfmt" },
                 },
