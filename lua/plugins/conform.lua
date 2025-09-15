5703c753a1be76a44ee84a03ad88a3f5ca7a9864
Illyasviel <zy99306@outlook.com>
2025-09-15 16:38:29 +0800

    adjust conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,24 +1,7 @@
--- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md
-vim.api.nvim_create_user_command("Format", function(args)
-    local range = nil
-    if args.count ~= -1 then
-        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-        range = {
-            start = { args.line1, 0 },
-            ["end"] = { args.line2, end_line:len() },
-        }
-    end
-    require("conform").format({ async = true, lsp_format = "fallback", range = range })
-end, { range = true })
-
-vim.api.nvim_create_user_command("TrimWhitespace", function()
-    require("conform").format({ formatters = { "trim_whitespace" } })
-end, {})
-
 return {
     {
         "stevearc/conform.nvim",
-        event = { "BufWritePre" },
+        lazy = false,
         cmd = { "ConformInfo" },
         config = function()
             local conform = require("conform")
@@ -37,9 +20,6 @@ return {
                     json = { "prettierd", "prettier", stop_after_first = true },
                     sh = { "shfmt" },
                 },
-                -- format_after_save = {
-                -- 	lsp_format = "fallback",
-                -- },
 
                 format_after_save = function(bufnr)
                     local bufname = vim.api.nvim_buf_get_name(bufnr)
@@ -52,25 +32,6 @@ return {
                     end
                     return { lsp_format = "fallback" }
                 end,
-
-                -- format_on_save = function(bufnr)
-                -- 	-- Disable autoformat on certain filetypes
-                -- 	local ignore_filetypes = { "sql", "java", "py" }
-                -- 	if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-                -- 		return
-                -- 	end
-                -- 	-- Disable with a global or buffer-local variable
-                -- 	if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                -- 		return
-                -- 	end
-                -- 	-- Disable autoformat for files in a certain path
-                -- 	local bufname = vim.api.nvim_buf_get_name(bufnr)
-                -- 	if bufname:match("/node_modules/") then
-                -- 		return
-                -- 	end
-                -- 	-- ...additional logic...
-                -- 	return { timeout_ms = 500, lsp_format = "fallback" }
-                -- end,
             })
             conform.formatters.shfmt = {
                 prepend_args = function(self, ctx)
@@ -82,6 +43,51 @@ return {
                     return { "--config-path", vim.fn.stdpath("config") .. "/stylua.toml" }
                 end,
             }
+
+            conform.setup({
+                format_on_save = function(bufnr)
+                    -- Disable with a global or buffer-local variable
+                    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                        return
+                    end
+                    return { timeout_ms = 500, lsp_format = "fallback" }
+                end,
+            })
+
+            vim.api.nvim_create_user_command("FormatDisable", function(args)
+                if args.bang then
+                    -- FormatDisable! will disable formatting just for this buffer
+                    vim.b.disable_autoformat = true
+                else
+                    vim.g.disable_autoformat = true
+                end
+            end, {
+                desc = "Disable autoformat-on-save",
+                bang = true,
+            })
+
+            vim.api.nvim_create_user_command("FormatEnable", function()
+                vim.b.disable_autoformat = false
+                vim.g.disable_autoformat = false
+            end, {
+                desc = "Re-enable autoformat-on-save",
+            })
+
+            vim.api.nvim_create_user_command("Format", function(args)
+                local range = nil
+                if args.count ~= -1 then
+                    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+                    range = {
+                        start = { args.line1, 0 },
+                        ["end"] = { args.line2, end_line:len() },
+                    }
+                end
+                require("conform").format({ async = true, lsp_format = "fallback", range = range })
+            end, { range = true })
+
+            vim.api.nvim_create_user_command("TrimWhitespace", function()
+                require("conform").format({ formatters = { "trim_whitespace" } })
+            end, {})
         end,
     },
 }
