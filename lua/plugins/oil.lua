558ccd87f718d0706fd51aa4ea6bb44e752a94e0
Illyasviel <zy99306@outlook.com>
2025-09-24 17:40:58 +0800

    adjust oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -18,8 +18,36 @@ return {
                 view_options = {
                     show_hidden = true,
                 },
+                keymaps = {
+					-- https://github.com/stevearc/oil.nvim/issues/454
+                    ["<C-p>"] = {
+                        callback = function()
+                            local oil = require("oil")
+							local util = require("oil.util")
+                            local entry = oil.get_cursor_entry()
+                            if not entry then
+                                vim.notify("Could not find entry under cursor", vim.log.levels.ERROR)
+                                return
+                            end
+                            local winid = util.get_preview_win()
+                            if winid then
+                                local cur_id = vim.w[winid].oil_entry_id
+                                if entry.id == cur_id then
+                                    vim.api.nvim_win_close(winid, true)
+                                    if util.is_floating_win() then
+                                        local layout = require("oil.layout")
+                                        local win_opts = layout.get_fullscreen_win_opts()
+                                        vim.api.nvim_win_set_config(0, win_opts)
+                                    end
+                                    return
+                                end
+                            end
+                            oil.open_preview { vertical = true, split = 'botright' }
+                        end,
+                    },
+                },
             })
-			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+            vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
         end,
     },
 }
