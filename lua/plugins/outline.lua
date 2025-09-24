47e2cd08dc4b07bc29347fb7b8da7287b9472268
Illyasviel <zy99306@outlook.com>
2025-09-24 10:10:31 +0800

    adjust outline

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -20,12 +20,26 @@ return {
     --     end,
     -- },
 
+    -- {
+    --     "oskarrrrrrr/symbols.nvim",
+    --     config = function()
+    --         local r = require("symbols.recipes")
+    --         require("symbols").setup(r.DefaultFilters, r.AsciiSymbols, {})
+    --         vim.keymap.set("n", "<leader>I", "<cmd>SymbolsToggle<CR>")
+    --     end,
+    -- },
+
     {
-        "oskarrrrrrr/symbols.nvim",
-        config = function()
-            local r = require("symbols.recipes")
-            require("symbols").setup(r.DefaultFilters, r.AsciiSymbols, {})
-            vim.keymap.set("n", "<leader>I", "<cmd>SymbolsToggle<CR>")
-        end,
+        "hedyhli/outline.nvim",
+        lazy = true,
+        cmd = { "Outline", "OutlineOpen" },
+        keys = {
+            { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+        },
+        opts = {
+            keymaps = {
+                goto_location = { "<CR>", "<LeftRelease>" },
+            },
+        },
     },
 }
