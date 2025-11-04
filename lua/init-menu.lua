6808f6a40b7efa58e1ef2d891b320c0b78084174
Illyasviel <zy99306@outlook.com>
2025-11-04 15:46:18 +0800

    update

--- lua/init-menu.lua
+++ lua/init-menu.lua
@@ -4,7 +4,7 @@ vim.cmd([[
   aunmenu PopUp.Select\ All
   aunmenu PopUp.-1-
 
-  amenu PopUp.Buffers <Cmd>:lua require("telescope.builtin").buffers(require('telescope.themes').get_cursor())<CR>
+  amenu PopUp.Buffers <Cmd>:lua require("telescope.builtin").buffers(require('telescope.themes').get_ivy())<CR>
   amenu PopUp.FileFormat <Cmd>:set fileformat?<CR>
 
   amenu PopUp.-100- <Nop>
