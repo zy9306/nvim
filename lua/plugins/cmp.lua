bd62c9c2972e6f39d5acfbc45835dd03fb312b3c
Illyasviel <zy99306@outlook.com>
2025-12-19 13:48:24 +0800

    adjust copilot keymap

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -5,20 +5,6 @@ return {
             keymap = {
                 preset = "default",
                 ["<CR>"] = { "accept", "fallback" },
-                ["<Tab>"] = {
-                    function(cmp)
-                        cmp.hide()
-                        require("copilot.suggestion").next()
-                        return true
-                    end,
-                },
-                ["<S-Tab>"] = {
-                    function(cmp)
-                        cmp.hide()
-                        require("copilot.suggestion").prev()
-                        return true
-                    end,
-                },
             },
 
             snippets = { preset = "luasnip" },
@@ -70,14 +56,40 @@ return {
                     auto_trigger = true,
                     keymap = {
                         accept = "<C-l>",
-                        accept_word = "<C-Right>",
-                        next = "<Tab>",
-                        prev = "<S-Tab>",
+                        accept_word = false,
+                        next = false,
+                        prev = false,
                         dismiss = "<C-]>",
                     },
                 },
             })
+
             local copilot_suggestion = require("copilot.suggestion")
+
+            vim.keymap.set("i", "<Tab>", function()
+                if copilot_suggestion.is_visible() then
+                    copilot_suggestion.accept_word()
+                else
+                    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", true)
+                end
+            end, { noremap = true, silent = true, desc = "Accept Copilot suggestion or insert Tab" })
+
+            vim.keymap.set("i", "<C-n>", function()
+                if copilot_suggestion.is_visible() then
+                    copilot_suggestion.next()
+                else
+                    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, false, true), "n", true)
+                end
+            end, { noremap = true, silent = true, desc = "Next Copilot suggestion or normal C-n" })
+
+            vim.keymap.set("i", "<C-p>", function()
+                if copilot_suggestion.is_visible() then
+                    copilot_suggestion.prev()
+                else
+                    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, false, true), "n", true)
+                end
+            end, { noremap = true, silent = true, desc = "Previous Copilot suggestion or normal C-p" })
+
             vim.keymap.set("i", "<C-c>", function()
                 if copilot_suggestion.is_visible() then
                     copilot_suggestion.dismiss()
