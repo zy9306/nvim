343a0d192ae73d2a0ec723e274f8378d2b47b20f
Illyasviel <zy99306@outlook.com>
2026-01-06 10:40:42 +0800

    disable toggleterm

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -16,84 +16,86 @@
 --     end,
 -- })
 
-return {
-    {
-        "akinsho/toggleterm.nvim",
-        event = "BufEnter",
-        version = "*",
-        config = true,
-        config = function()
-            require("toggleterm").setup({
-                size = function(term)
-                    if term.direction == "horizontal" then
-                        return math.floor(vim.o.lines * 0.4)
-                    elseif term.direction == "vertical" then
-                        return math.floor(vim.o.columns * 0.3)
-                    end
-                end,
-                start_in_insert = true,
-                direction = "vertical",
-            })
-            local opts = { noremap = true, silent = true }
+-- return {
+--     {
+--         "akinsho/toggleterm.nvim",
+--         event = "BufEnter",
+--         version = "*",
+--         config = true,
+--         config = function()
+--             require("toggleterm").setup({
+--                 size = function(term)
+--                     if term.direction == "horizontal" then
+--                         return math.floor(vim.o.lines * 0.4)
+--                     elseif term.direction == "vertical" then
+--                         return math.floor(vim.o.columns * 0.3)
+--                     end
+--                 end,
+--                 start_in_insert = true,
+--                 direction = "vertical",
+--             })
+--             local opts = { noremap = true, silent = true }
+--
+--             -- command start
+--             vim.api.nvim_create_user_command("CreateTermHorizontal", function()
+--                 require("toggleterm.terminal").Terminal:new({ direction = "horizontal" }):toggle()
+--             end, {})
+--
+--             vim.api.nvim_create_user_command("CreateTermVertical", function()
+--                 require("toggleterm.terminal").Terminal:new({ direction = "vertical" }):toggle()
+--             end, {})
+--
+--             vim.api.nvim_create_user_command("ToggleTermHorizontal", function()
+--                 vim.cmd("ToggleTerm direction=horizontal")
+--             end, {})
+--
+--             vim.api.nvim_create_user_command("ToggleTermVertical", function()
+--                 vim.cmd("ToggleTerm direction=vertical")
+--             end, {})
+--             -- command end
+--
+--             vim.keymap.set("n", "<F6>", ":ToggleTerm direction=float<CR>", opts)
+--             vim.keymap.set("t", "<F6>", "<C-\\><C-n>:ToggleTerm<CR>", opts)
+--
+--             vim.keymap.set("t", "<C-Up>", "<C-\\><C-n>:resize -2<CR>", opts)
+--             vim.keymap.set("t", "<C-Down>", "<C-\\><C-n>:resize +2<CR>", opts)
+--             vim.keymap.set("t", "<C-Left>", "<C-\\><C-n>:vertical resize -2<CR>", opts)
+--             vim.keymap.set("t", "<C-Right>", "<C-\\><C-n>:vertical resize +2<CR>", opts)
+--
+--             vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+--
+--             local terms = require("toggleterm.terminal")
+--             vim.api.nvim_create_autocmd("BufEnter", {
+--                 pattern = "term://*",
+--                 callback = function()
+--                     if vim.fn.mode() ~= "t" then
+--                         local _, term = terms.identify()
+--                         if term then
+--                             term:set_mode(terms.mode.INSERT)
+--                         end
+--                     end
+--                 end,
+--             })
+--
+--             vim.api.nvim_create_autocmd("ModeChanged", {
+--                 pattern = "v:*",
+--                 callback = function()
+--                     if vim.bo.buftype == "terminal" then
+--                         local _, term = terms.identify()
+--                         if term then
+--                             term:set_mode(terms.mode.INSERT)
+--                         end
+--                     end
+--                 end,
+--             })
+--
+--             vim.api.nvim_create_autocmd("TermOpen", {
+--                 callback = function()
+--                     vim.keymap.set("n", "q", "<Cmd>startinsert<CR>", { buffer = true, noremap = true, silent = true })
+--                 end,
+--             })
+--         end,
+--     },
+-- }
 
-            -- command start
-            vim.api.nvim_create_user_command("CreateTermHorizontal", function()
-                require("toggleterm.terminal").Terminal:new({ direction = "horizontal" }):toggle()
-            end, {})
-
-            vim.api.nvim_create_user_command("CreateTermVertical", function()
-                require("toggleterm.terminal").Terminal:new({ direction = "vertical" }):toggle()
-            end, {})
-
-            vim.api.nvim_create_user_command("ToggleTermHorizontal", function()
-                vim.cmd("ToggleTerm direction=horizontal")
-            end, {})
-
-            vim.api.nvim_create_user_command("ToggleTermVertical", function()
-                vim.cmd("ToggleTerm direction=vertical")
-            end, {})
-            -- command end
-
-            vim.keymap.set("n", "<F6>", ":ToggleTerm direction=float<CR>", opts)
-            vim.keymap.set("t", "<F6>", "<C-\\><C-n>:ToggleTerm<CR>", opts)
-
-            vim.keymap.set("t", "<C-Up>", "<C-\\><C-n>:resize -2<CR>", opts)
-            vim.keymap.set("t", "<C-Down>", "<C-\\><C-n>:resize +2<CR>", opts)
-            vim.keymap.set("t", "<C-Left>", "<C-\\><C-n>:vertical resize -2<CR>", opts)
-            vim.keymap.set("t", "<C-Right>", "<C-\\><C-n>:vertical resize +2<CR>", opts)
-
-            vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
-
-            local terms = require("toggleterm.terminal")
-            vim.api.nvim_create_autocmd("BufEnter", {
-                pattern = "term://*",
-                callback = function()
-                    if vim.fn.mode() ~= "t" then
-                        local _, term = terms.identify()
-                        if term then
-                            term:set_mode(terms.mode.INSERT)
-                        end
-                    end
-                end,
-            })
-
-            vim.api.nvim_create_autocmd("ModeChanged", {
-                pattern = "v:*",
-                callback = function()
-                    if vim.bo.buftype == "terminal" then
-                        local _, term = terms.identify()
-                        if term then
-                            term:set_mode(terms.mode.INSERT)
-                        end
-                    end
-                end,
-            })
-
-            vim.api.nvim_create_autocmd("TermOpen", {
-                callback = function()
-                    vim.keymap.set("n", "q", "<Cmd>startinsert<CR>", { buffer = true, noremap = true, silent = true })
-                end,
-            })
-        end,
-    },
-}
+return {}
