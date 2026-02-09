35c3cc11fd66621f1210d25b8f35a3cbf02d3137
Illyasviel <zy99306@outlook.com>
2026-02-09 15:33:26 +0800

    Let smart-splits own <C-S-hjkl> navigation for consistency

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -2,15 +2,15 @@ return {
     {
         "mrjones2014/smart-splits.nvim",
         config = function()
-            -- vim.keymap.set("n", "<C-S-h>", require("smart-splits").move_cursor_left)
-            -- vim.keymap.set("n", "<C-S-j>", require("smart-splits").move_cursor_down)
-            -- vim.keymap.set("n", "<C-S-k>", require("smart-splits").move_cursor_up)
-            -- vim.keymap.set("n", "<C-S-l>", require("smart-splits").move_cursor_right)
-            --
-            -- vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
-            -- vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
-            -- vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
-            -- vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
+            vim.keymap.set("n", "<C-S-h>", require("smart-splits").move_cursor_left)
+            vim.keymap.set("n", "<C-S-j>", require("smart-splits").move_cursor_down)
+            vim.keymap.set("n", "<C-S-k>", require("smart-splits").move_cursor_up)
+            vim.keymap.set("n", "<C-S-l>", require("smart-splits").move_cursor_right)
+
+            vim.keymap.set("t", "<C-S-h>", require("smart-splits").move_cursor_left)
+            vim.keymap.set("t", "<C-S-j>", require("smart-splits").move_cursor_down)
+            vim.keymap.set("t", "<C-S-k>", require("smart-splits").move_cursor_up)
+            vim.keymap.set("t", "<C-S-l>", require("smart-splits").move_cursor_right)
         end,
     },
 }
--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -8,15 +8,15 @@ return {
                 },
             })
 
-            vim.keymap.set("n", "<C-S-h>", require("tmux").move_left)
-            vim.keymap.set("n", "<C-S-j>", require("tmux").move_bottom)
-            vim.keymap.set("n", "<C-S-k>", require("tmux").move_top)
-            vim.keymap.set("n", "<C-S-l>", require("tmux").move_right)
+            -- vim.keymap.set("n", "<C-S-h>", require("tmux").move_left)
+            -- vim.keymap.set("n", "<C-S-j>", require("tmux").move_bottom)
+            -- vim.keymap.set("n", "<C-S-k>", require("tmux").move_top)
+            -- vim.keymap.set("n", "<C-S-l>", require("tmux").move_right)
 
-            vim.keymap.set("t", "<C-S-h>", require("tmux").move_left)
-            vim.keymap.set("t", "<C-S-j>", require("tmux").move_bottom)
-            vim.keymap.set("t", "<C-S-k>", require("tmux").move_top)
-            vim.keymap.set("t", "<C-S-l>", require("tmux").move_right)
+            -- vim.keymap.set("t", "<C-S-h>", require("tmux").move_left)
+            -- vim.keymap.set("t", "<C-S-j>", require("tmux").move_bottom)
+            -- vim.keymap.set("t", "<C-S-k>", require("tmux").move_top)
+            -- vim.keymap.set("t", "<C-S-l>", require("tmux").move_right)
         end,
     },
 }
