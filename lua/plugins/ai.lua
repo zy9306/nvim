2e21e05bc394f65d53da4104336f43500b44f58c
Illyasviel <zy99306@outlook.com>
2026-03-16 10:53:47 +0800

    Disable opencode plugin config
    Comment out the opencode.nvim setup so it no longer loads while keeping the previous configuration around for later reuse.

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,32 +1,32 @@
 return {
-    {
-        "NickvanDyke/opencode.nvim",
-        dependencies = {
-            { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
-        },
-        config = function()
-            vim.keymap.set({ "n", "x" }, "<C-a>", function()
-                require("opencode").ask("@this: ", { submit = true })
-            end, { desc = "Ask opencode" })
-
-            vim.keymap.set({ "n", "x" }, "<C-x>", function()
-                require("opencode").select()
-            end, { desc = "Execute opencode action…" })
-
-            vim.keymap.set({ "n", "x" }, "ga", function()
-                require("opencode").prompt("@this")
-            end, { desc = "Add to opencode" })
-
-            vim.keymap.set({ "n", "t" }, "<f5>", function()
-                require("opencode").toggle()
-            end, { desc = "Toggle opencode" })
-
-            vim.keymap.set({ "n" }, "<C-.>", function()
-                require("opencode").toggle()
-            end, { desc = "Toggle opencode" })
-
-            vim.keymap.set("n", "+", "<C-a>", { desc = "Increment", noremap = true })
-            vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement", noremap = true })
-        end,
-    },
+    -- {
+    --     "NickvanDyke/opencode.nvim",
+    --     dependencies = {
+    --         { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+    --     },
+    --     config = function()
+    --         vim.keymap.set({ "n", "x" }, "<C-a>", function()
+    --             require("opencode").ask("@this: ", { submit = true })
+    --         end, { desc = "Ask opencode" })
+    --
+    --         vim.keymap.set({ "n", "x" }, "<C-x>", function()
+    --             require("opencode").select()
+    --         end, { desc = "Execute opencode action…" })
+    --
+    --         vim.keymap.set({ "n", "x" }, "ga", function()
+    --             require("opencode").prompt("@this")
+    --         end, { desc = "Add to opencode" })
+    --
+    --         vim.keymap.set({ "n", "t" }, "<f5>", function()
+    --             require("opencode").toggle()
+    --         end, { desc = "Toggle opencode" })
+    --
+    --         vim.keymap.set({ "n" }, "<C-.>", function()
+    --             require("opencode").toggle()
+    --         end, { desc = "Toggle opencode" })
+    --
+    --         vim.keymap.set("n", "+", "<C-a>", { desc = "Increment", noremap = true })
+    --         vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement", noremap = true })
+    --     end,
+    -- },
 }
