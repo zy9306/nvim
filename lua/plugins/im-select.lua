d9dd0e4a1995281e61781dcc50ae692e3d2ccfc3
Illyasviel <zy99306@outlook.com>
2026-05-14 16:26:20 +0800

    Configure markdown rendering and input method plugin

---
+++ lua/plugins/im-select.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        "keaising/im-select.nvim",
+        config = function()
+            require("im_select").setup({})
+        end,
+    },
+}
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,11 +2,26 @@ return {
     {
         "MeanderingProgrammer/render-markdown.nvim",
         opts = {
-			enabled = false,
+            enabled = true,
             file_types = { "markdown" },
             anti_conceal = { enabled = false },
-            indent = {
+            heading = { enabled = false },
+            -- paragraph = { enabled = false },
+            -- code = { enabled = false },
+            -- dash = { enabled = false },
+            -- document = { enabled = false },
+            -- bullet = { enabled = false },
+            -- checkbox = { enabled = false },
+            -- quote = { enabled = false },
+            -- callout = { enabled = false },
+            -- link = { enabled = false },
+            -- latex = { enabled = false },
+            -- html = { enabled = false },
+            indent = { enabled = false },
+            -- sign = { enabled = false },
+            pipe_table = {
                 enabled = true,
+                style = "full",
             },
         },
     },
